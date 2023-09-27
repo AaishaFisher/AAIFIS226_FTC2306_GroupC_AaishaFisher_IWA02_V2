e12d1836169a0ba40b25683b14a312e56ac38bf2
Aai'sha Fisher <aaishafisher484@gmail.com>
2023-09-27 09:32:30 +0200

    Completed.

---
+++ Challenge 2/script.js
@@ -0,0 +1,15 @@
+// (c) ACME Inc. 2010
+
+/** 
+ * This is the date that a user created their account 
+ */
+
+const date = '10/07/2014';
+
+console.log(date);
+
+
+
+
+//Notes:
+//.log was missing!
